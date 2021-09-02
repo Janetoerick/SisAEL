4ab0c8bc0ec13eb56f24c701c6771dba54a164f8
Janetoerick <janetoerick18@gmail.com>
2021-09-02 18:24:14 -0300

    Melhorando solução de ProdutoValidator para laboratorio

--- src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidatorImpl.java
+++ src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidatorImpl.java
@@ -1,39 +1,28 @@
 package com.ufrn.cb.SisAEL.service;
 
+import org.springframework.stereotype.Service;
+
 import com.ufrn.cb.SisAEL.entity.Equipamento;
 import com.ufrn.cb.SisAEL.entity.Laboratorio;
 import com.ufrn.cb.SisAEL.entity.Produto;
 import com.ufrn.cb.SisAEL.entity.Sala;
 
+@Service
 public class ProdutoValidatorImpl extends ProdutoValidator{
 
 	public boolean validar(Produto produto) {
-		try {
+		
+		if(produto instanceof Equipamento) {
 			Equipamento e = (Equipamento) produto;
-			
-			if(e.getTombamento() != null) {
-				EquipamentoValide ev = new EquipamentoValide();
-				return ev.validar(produto);
-			}
-		} catch(Exception ex) {
-			try {
-				Laboratorio l = (Laboratorio) produto;
-				if(l.getSalas() != null) {
-					LaboratorioValide lv = new LaboratorioValide();
-					return lv.validar(produto);
-				}
-				
-			} catch(Exception ex1) {
-				try {
-					Sala s = (Sala) produto;
-					SalaValide sv = new SalaValide();
-					return sv.validar(produto);
-				} catch(Exception ex2) {
-					return false;
-				}
-				
-			}
+			return e.getTombamento() > 0;
+		} else if (produto instanceof Sala) {
+			Sala s = (Sala) produto;
+			return s.getNome() != null;
+		} else if (produto instanceof Laboratorio) {
+			Laboratorio l = (Laboratorio) produto;
+			return l.getNome() != null && l.getSalas() != null;
 		}
+		
 		return false;
 	}
 }
