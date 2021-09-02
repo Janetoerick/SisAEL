f71cfc72270187a2fa2cd35d0329bf197ae82336
Janetoerick <janetoerick18@gmail.com>
2021-09-02 00:45:26 -0300

    Resolvido problema de verificacao de varios produtos

--- src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidator.java
+++ src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidator.java
@@ -2,42 +2,11 @@ package com.ufrn.cb.SisAEL.service;
 
 import org.springframework.stereotype.Service;
 
-import com.ufrn.cb.SisAEL.entity.Equipamento;
-import com.ufrn.cb.SisAEL.entity.Laboratorio;
 import com.ufrn.cb.SisAEL.entity.Produto;
-import com.ufrn.cb.SisAEL.entity.Sala;
 
 @Service
-public class ProdutoValidator {
+public abstract class ProdutoValidator {
 
-	public boolean validar(Produto produto) {
-		try {
-			Equipamento e = (Equipamento) produto;
-			
-			if(e.getTombamento() != null) {
-				EquipamentoValide ev = new EquipamentoValide();
-				return ev.validar(produto);
-			}
-		} catch(Exception ex) {
-			try {
-				Laboratorio l = (Laboratorio) produto;
-				if(l.getSalas() != null) {
-					LaboratorioValide lv = new LaboratorioValide();
-					return lv.validar(produto);
-				}
-				
-			} catch(Exception ex1) {
-				try {
-					Sala s = (Sala) produto;
-					SalaValide sv = new SalaValide();
-					return sv.validar(produto);
-				} catch(Exception ex2) {
-					return false;
-				}
-				
-			}
-		}
-		return false;
-	}
+	public abstract boolean validar(Produto produto);
 
 }
--- src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidatorImpl.java
+++ src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidatorImpl.java
@@ -1,5 +1,39 @@
 package com.ufrn.cb.SisAEL.service;
 
-public class ProdutoValidatorImpl {
+import com.ufrn.cb.SisAEL.entity.Equipamento;
+import com.ufrn.cb.SisAEL.entity.Laboratorio;
+import com.ufrn.cb.SisAEL.entity.Produto;
+import com.ufrn.cb.SisAEL.entity.Sala;
 
+public class ProdutoValidatorImpl extends ProdutoValidator{
+
+	public boolean validar(Produto produto) {
+		try {
+			Equipamento e = (Equipamento) produto;
+			
+			if(e.getTombamento() != null) {
+				EquipamentoValide ev = new EquipamentoValide();
+				return ev.validar(produto);
+			}
+		} catch(Exception ex) {
+			try {
+				Laboratorio l = (Laboratorio) produto;
+				if(l.getSalas() != null) {
+					LaboratorioValide lv = new LaboratorioValide();
+					return lv.validar(produto);
+				}
+				
+			} catch(Exception ex1) {
+				try {
+					Sala s = (Sala) produto;
+					SalaValide sv = new SalaValide();
+					return sv.validar(produto);
+				} catch(Exception ex2) {
+					return false;
+				}
+				
+			}
+		}
+		return false;
+	}
 }
