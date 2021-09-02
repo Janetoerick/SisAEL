74f24c398cfd9a72cd93ab959666a9ec302b25c2
Janetoerick <janetoerick18@gmail.com>
2021-09-02 00:44:57 -0300

    Resolvido problema de verificação de varios produtos

--- src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidator.java
+++ src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidator.java
@@ -1,9 +1,43 @@
 package com.ufrn.cb.SisAEL.service;
 
-import com.ufrn.cb.SisAEL.entity.Produto;
+import org.springframework.stereotype.Service;
 
-public abstract class ProdutoValidator {
-	
-	public abstract boolean validar(Produto produto);
+import com.ufrn.cb.SisAEL.entity.Equipamento;
+import com.ufrn.cb.SisAEL.entity.Laboratorio;
+import com.ufrn.cb.SisAEL.entity.Produto;
+import com.ufrn.cb.SisAEL.entity.Sala;
+
+@Service
+public class ProdutoValidator {
+
+	public boolean validar(Produto produto) {
+		try {
+			Equipamento e = (Equipamento) produto;
+			
+			if(e.getTombamento() != null) {
+				EquipamentoValide ev = new EquipamentoValide();
+				return ev.validar(produto);
+			}
+		} catch(Exception ex) {
+			try {
+				Laboratorio l = (Laboratorio) produto;
+				if(l.getSalas() != null) {
+					LaboratorioValide lv = new LaboratorioValide();
+					return lv.validar(produto);
+				}
+				
+			} catch(Exception ex1) {
+				try {
+					Sala s = (Sala) produto;
+					SalaValide sv = new SalaValide();
+					return sv.validar(produto);
+				} catch(Exception ex2) {
+					return false;
+				}
+				
+			}
+		}
+		return false;
+	}
 
 }
--- src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidatorImpl.java
+++ src/main/java/com/ufrn/cb/SisAEL/service/ProdutoValidatorImpl.java
@@ -1,19 +1,5 @@
 package com.ufrn.cb.SisAEL.service;
 
-import org.springframework.stereotype.Service;
-
-import com.ufrn.cb.SisAEL.entity.Laboratorio;
-import com.ufrn.cb.SisAEL.entity.Produto;
-
-import javassist.expr.Instanceof;
-
-@Service
-public class ProdutoValidatorImpl extends ProdutoValidator{
-
-	@Override
-	public boolean validar(Produto produto) {
-		
-		return true;
-	}
+public class ProdutoValidatorImpl {
 
 }
