6f754d0e4b554284df85feb733e5a18734136da2
Janetoerick <janetoerick18@gmail.com>
2021-09-14 13:59:48 -0300

    hotel totalmente implementado

--- src/main/java/com/ufrn/cb/SisAEL/service/VerificadorProdutos.java
+++ src/main/java/com/ufrn/cb/SisAEL/service/VerificadorProdutos.java
@@ -2,12 +2,17 @@ package com.ufrn.cb.SisAEL.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.ufrn.cb.SisAEL.dados.FachadaDados;
 import com.ufrn.cb.SisAEL.entity.Produto;
 
 @Service
 public abstract class VerificadorProdutos {
+	
+	@Autowired
+	protected FachadaDados fachada;
 	
 	public abstract boolean verificar(List<Produto> produtos);
 
